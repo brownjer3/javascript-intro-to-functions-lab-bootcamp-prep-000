b48f21a958ed5b33f712a9c27c6278e4ee7a59a3
Jerry Brown <brownjer3@gmail.com>
2018-02-17 21:31:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var uppercase = "HELLO!"
-var lowercase = "hello!"
+
 
 function shout(string) {
   return string.toUpperCase()
@@ -18,7 +18,8 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  
+ var lowercase = "hello!"
+ lowercase.toLowerCase() === lowerase
  return "I can\'t hear you!"
 }
 
